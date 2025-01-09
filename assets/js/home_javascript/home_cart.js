39b4247e16658f1fbf11efaba8dcd76289f006aa
MostafaKadry <mostafakadry806@gmail.com>
2025-01-10 00:18:17 +0200

    add to cart fn in home page && view cart in HP

--- assets/js/home_javascript/home_cart.js
+++ assets/js/home_javascript/home_cart.js
@@ -1,5 +1,7 @@
 // open and close card
-
+let cartInStorage = JSON.parse(localStorage.getItem("cart")) || [] ;
+let openCartPageBTN = document.getElementById("open-cart-page"),
+    addToCartIcon = document.getElementById("add-to-cart-icon");
 var cart = document.querySelector('.cart');
 function open_cart()
 {
@@ -9,3 +11,18 @@ function close_cart()
 {
     cart.classList.remove("active")
 }
+
+
+// please accept this changes -- Mostafa K.
+openCartPageBTN.addEventListener("click", () => {
+    window.location.href = "./cart.html";
+})
+addToCartIcon.addEventListener('click', setCartProduct);
+
+function setCartProduct({ id, name, price, description, image }) {
+    if (!id || !name || !price || !description || !image)
+      return window.alert("There are some thing wrong here!");
+  
+    cart.push({ id, name, price, description, image, qty: 1 });
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
