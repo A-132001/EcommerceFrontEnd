05b49b7099761e41c9b2e547162e57c4ab25f7bc
bassem beshay <bassembeshay50@gmail.com>
2025-01-12 15:31:00 +0200

    add brands in productstore

--- assets/js/productStore.js
+++ assets/js/productStore.js
@@ -1,89 +1,118 @@
 xhr = new XMLHttpRequest();
 xhr.open("GET", "https://fake-products-api-kappa.vercel.app/api/products");
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let productDetails = [];
 
 var data = [];
 var elements = document.getElementById("productData");
-
+var members = document.getElementById("store-link");
 
 document.addEventListener("DOMContentLoaded", function () {
-  xhr.open("GET", "https://fake-products-api-kappa.vercel.app/api/products");
   xhr.onload = function () {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       console.log(response);
       data = response;
-      const storeData = JSON.parse(localStorage.getItem("storeDate")) || response.store1.products;
+
+      const storeData = JSON.parse(localStorage.getItem("cart")) || response.store1.products;
       console.log(storeData);
-      
-      displayProductsStore1(storeData);
-      // displayName();
+
+      showProductss("store1");
+      displayName();
+    } else {
+      console.error("Failed to fetch data from API.");
     }
   };
   xhr.send();
 });
 
-
-function displayProductsStore1(products) {
-  products.forEach((product) => {
-    
-    var element1 = document.createElement("div");
-    element1.classList.add("stylediv");
-    element1.innerHTML = `
-    <h3>${product.name}</h3>
-    <h5>${product.description}</h5>
-    <img class='productImage' src ="${product.image}" width="250px">
-    <h6> ${product.price} $</h6>
-    `;
-    elements.appendChild(element1);
-    var element3 = document.createElement("button");
-    var element4 = document.createElement("p");
-    element4.innerHTML = "&#9825";
-    element1.appendChild(element4);
-    element3.textContent = `add to cart`;
-    element3.style.display = "none";
-    element4.style.display = "none";
-    element1.appendChild(element3);
-    element3.classList.add("buttonStyle");
-    element4.classList.add("heartStyle");
-    function buttonAdd() {
-      element4.style.display = "block";
-      element3.style.display = "block";
+function displayName() {
+  var element = document.createElement("h2");
+  element.classList.add("styleH2");
+  let links = '';
+  for (let index = 1; index <= Object.keys(data).length; index++) {
+    const storeKey = `store${index}`;
+    if (data[storeKey]) {
+      links += `<a href="#" class="store-link" onclick="showProductss('${storeKey}')">${data[storeKey].name}</a>`;
     }
-    function buttondel() {
+  }
+  element.innerHTML = links;
+  members.appendChild(element);
+}
+
+function showProductss(storeId) {
+  elements.innerHTML = '';
+
+  const storeData = data[storeId];
+  if (storeData && storeData.products) {
+    storeData.products.forEach((product) => {
+      var element1 = document.createElement("div");
+      element1.classList.add("stylediv");
+
+      element1.innerHTML = `
+        <h3>${product.name}</h3>
+        <h5>${product.description}</h5>
+        <img class='productImage' src ="${product.image}" width="250px">
+        <h6>${product.price} $</h6>
+      `;
+
+      elements.appendChild(element1);
+
+      var element3 = document.createElement("button");
+      var element4 = document.createElement("p");
+      element4.innerHTML = "&#9825";
+      element1.appendChild(element4);
+
+      element3.textContent = `Add to Cart`;
       element3.style.display = "none";
       element4.style.display = "none";
-    }
-    element3.onclick = function () {
-      setCartProduct({
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        description: product.description,
-        image: product.image,
-      });
-    };
-    // Display product details and send data by local storge when clicked // Added by Abdo
-    var productImage = element1.querySelector(".productImage");
-    productImage.onclick = function () {
-      sendDataToProductDetails({
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        description: product.description,
-        image: product.image,
-      });
-      window.location.href = `product.html`;
-    };
+      element1.appendChild(element3);
 
-    element1.onmouseenter = buttonAdd;
-    element1.onmouseleave = buttondel;
-  });
+      element3.classList.add("buttonStyle");
+      element4.classList.add("heartStyle");
+
+      function buttonAdd() {
+        element4.style.display = "block";
+        element3.style.display = "block";
+      }
+      function buttondel() {
+        element3.style.display = "none";
+        element4.style.display = "none";
+      }
+
+      element3.onclick = function () {
+        setCartProduct({
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          image: product.image,
+        });
+      };
+
+      var productImage = element1.querySelector(".productImage");
+      productImage.onclick = function () {
+        sendDataToProductDetails({
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          image: product.image,
+        });
+        window.location.href = `product.html`;
+      };
+
+      element1.onmouseenter = buttonAdd;
+      element1.onmouseleave = buttondel;
+    });
+  } else {
+    console.error(`Store data for ${storeId} not found.`);
+  }
 }
 
 function setCartProduct({ id, name, price, description, image }) {
   if (!id || !name || !price || !description || !image)
-    return window.alert("There are some thing wrong here!");
+    return window.alert("There is something wrong here!");
 
   const isProductInCart = cart.some((product) => product.id === id);
 
@@ -95,15 +124,9 @@ function setCartProduct({ id, name, price, description, image }) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// Added by Abdo
-let productDetails = [];
 function sendDataToProductDetails({ id, name, price, description, image }) {
   if (!id || !name || !price || !description || !image)
-    return window.alert("There are some thing wrong here!");
+    return window.alert("There is something wrong here!");
   productDetails.push({ id, name, price, description, image, qty: 1 });
   localStorage.setItem("productDetails", JSON.stringify(productDetails));
 }
-
-
-
-
